bcfecfeb8d7e3a0a26b4a9c99c026aee2d249a6f
kcooler <kcooler@godaddy.com>
2023-11-18 10:36:45 -0500

    Read input file and print contents

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env npx ts-node --esm
+import fs from 'fs';
 
-console.log("Typescript executed successfully");
+if (process.argv.length < 3) {
+    console.log('Usage: node dist/index.js <path_to_input_file>');
+    process.exit(1);
+}
+const filename = process.argv[2];
+console.log('Received input file: ', filename);
+
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
